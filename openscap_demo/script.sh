8e176e5479941d64ebef22d6b10dacf549024156
Dennis McCarthy <dmccuk@users.noreply.github.com>
2020-04-03 15:45:44 +0100

    Update script.sh

--- openscap_demo/script.sh
+++ openscap_demo/script.sh
@@ -45,7 +45,7 @@ openscap_report()
 {
 echo "*******************  Run openscap report"
 sudo oscap xccdf eval \
- --profile xccdf_org.ssgproject.content_profile_ospp42 \
+ --profile xccdf_org.ssgproject.content_profile_rht-ccp \
  --results-arf arf.xml \
  --report common-report.html \
  /usr/share/xml/scap/ssg/content/ssg-rhel7-ds.xml
